45194428063ebf66e190e939f53feb3815e63c05
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-02 12:06:52 +0100

    add jour03

---
+++ jour01/job03/index.php
@@ -0,0 +1,38 @@
+<?php 
+$bool ="true";
+$int ="8";
+$string = "Hello Wolrd!";
+$float = "2,3";
+
+    echo 
+        "<table>
+            <thead>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Valeur</th>
+            </thead>
+            <tboby>
+                <tr>
+                    <td>Boolean</td>
+                    <td>bool</td>
+                    <td>true</td>
+                </tr>
+                <tr>
+                    <td>INT</td>
+                    <td>int</td>
+                    <td>8</td>
+                </tr>
+                <tr>
+                    <td>String</td>
+                    <td>string</td>
+                    <td>Hello World!</td>
+                </tr>
+                <tr>
+                    <td>float</td>
+                    <td>float</td>
+                    <td>2,3</td>
+                </tr>
+            </tbody
+        </table>"
+    ;
+?>

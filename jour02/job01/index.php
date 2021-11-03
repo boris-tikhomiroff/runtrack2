17757133fd8dd7024caaee3ed9d49066559d83dd
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-03 11:03:22 +0100

    job01 done

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,12 +1,26 @@
 <?php
-for( $i = 0; $i <= 1337; $i++ ) {
-    echo $i . '<br />';
+$i = 0; 
 
-    if ($i === 41) {
+while ($i < 1338) // Boucle While
+{
+    echo $i++ . '<br />';
+
+    if ($i == 42) {
         echo '<b><u>';
     }
-    elseif ($i >= 42) {
+    else {
         echo '</b></u>';
     }
 }
+
+// for( $i = 0; $i <= 1337; $i++) { // Boucle for
+//     echo $i . '<br />';
+
+//     if ($i === 41) {
+//         echo '<b><u>';
+//     }
+//     else {
+//         echo '</b></u>';
+//     }
+// }
 ?>

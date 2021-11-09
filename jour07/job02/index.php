255b9af2299b4b5217f424037a6673f87291396d
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-09 12:09:48 +0100

    job02 done

---
+++ jour07/job02/index.php
@@ -0,0 +1,28 @@
+<?php
+// setcookie('nbvisiteurs', 0, time()+43200);
+// $_COOKIE['nbvisiteurs'] = $_COOKIE['nbvisiteurs']+1;
+
+// echo $_COOKIE['nbvisiteurs'];
+
+// if(isset($_POST['reset'])==true){
+//     setcookie('nbvisiteurs');
+// }
+
+if(!isset($_COOKIE['nbvisiteurs'])){
+    setcookie('nbvisiteurs', 1, time()+43200);
+}
+else {
+    setcookie('nbvisiteurs',$_COOKIE['nbvisiteurs']+1, time()+43200);
+    echo $_COOKIE['nbvisiteurs'];
+}
+
+if(isset($_POST['reset'])==true){
+    setcookie('nbvisiteurs');
+}
+
+?>
+
+<form action="index.php" method="post">
+    <label for="reset"></label>
+    <input type="submit" name="reset" value="reset" id="reset">
+</form>

bc67257747a22f3b829e0bbd365a4d8516575683
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-11 17:49:25 +0100

    option entrez un nombre

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,17 +1,17 @@
-
-
 <form action="index.php" method="get">
-    <label for="nombre">Nombre :</label>
-    <input type="number" name="nombre" id="nombre">
-    <input type="submit" name="submit" value="submit">
+    <label for="nombre">Votre nombre:</label>
+    <input type="text" name="nombre" id="nombre">
+    <label for="envoyer"></label>
+    <input type="submit" name="envoyer" id="envoyer" value="envoyer">
 </form>
 
 <?php
-
-if($_GET['nombre'] % 2 == 0) {
-    echo $_GET['nombre']. "Nombre pair";
+if(!isset($_GET['nombre'])){
+    echo "Entrez un nombre";
+}
+elseif($_GET['nombre'] % 2 == 0){
+    echo $_GET['nombre']."Nombre pair";
 }
 else{
-    echo $_GET['nombre'] . "Nombre impair";
+    echo $_GET['nombre']."Nombre impaire";
 }
-?>

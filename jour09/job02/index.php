80ff8e167dc922b1946f951f2679f5e9feec1bb0
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-12 14:30:41 +0100

    job02 rectif

--- jour09/job02/index.php
+++ jour09/job02/index.php
@@ -8,16 +8,20 @@ $salles = mysqli_fetch_all($requete, MYSQLI_ASSOC);
 <table>
     <thead>
         <tr>
+            <th>id</th>
             <th>Nom</th>
+            <th>id_étage</th>
             <th>Capacités</th>
         </tr>
         <tbody>
-        <?php
-            foreach($salles as $salle){
-                echo '<tr><td>'.$salle['nom'].'</td>';
-                echo '<td>'.$salle['capacite'].'</td></tr>';
-            };
-        ?>
+        <?php foreach($salles as $salle): ?>
+            <tr>
+                <td></td>
+                <td><?=$salle['nom'];?></td>
+                <td></td>
+                <td><?=$salle['capacite'];?></td>
+            </tr>
+        <?php endforeach;?>
         </tbody>
     </thead>
 </table>

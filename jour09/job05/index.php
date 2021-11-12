bd5e521cb07d2f03bc65ca1fcf543843e1e706a5
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-12 13:23:50 +0100

    job05 done

---
+++ jour09/job05/index.php
@@ -0,0 +1,28 @@
+<?php
+$bdd = mysqli_connect('localhost','root','','jour08');
+mysqli_set_charset($bdd, "utf8");
+$requete = mysqli_query($bdd, "SELECT * FROM `etudiants` WHERE DATEDIFF (CURRENT_DATE, `naissance`) >= 6570");
+$etudiants = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+?>
+<table>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>naissance</th>
+            <th>sexe</th>
+            <th>email</th>
+        </tr>
+        <tbody>
+        <?php
+            foreach($etudiants as $etudiant){
+                echo '<tr><td>'.$etudiant['nom'].'</td>';
+                echo '<td>'.$etudiant['prenom'].'</td>';
+                echo '<td>'.$etudiant['naissance'].'</td>';
+                echo '<td>'.$etudiant['sexe'].'</td>';
+                echo '<td>'.$etudiant['email'].'</td></tr>';
+            };
+        ?>
+        </tbody>
+    </thead>
+</table>

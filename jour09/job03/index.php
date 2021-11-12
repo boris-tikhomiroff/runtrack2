36db9cb3bd8067b391e13a69a0d5fcf407eaca2b
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-11-12 14:35:28 +0100

    job03 rectif

--- jour09/job03/index.php
+++ jour09/job03/index.php
@@ -2,23 +2,29 @@
 $bdd = mysqli_connect('localhost','root','','jour08');
 mysqli_set_charset($bdd, "utf8");
 $requete = mysqli_query($bdd, "SELECT * FROM `etudiants` WHERE `sexe` = 'Femme'");
-$etudiants = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+$student = mysqli_fetch_all($requete, MYSQLI_ASSOC);
 ?>
 <table>
     <thead>
         <tr>
-            <th>Nom</th>
-            <th>Prenom</th>
-            <th>naisance</th>
+            <th>id</th>
+            <th>prenom</th>
+            <th>nom</th>
+            <th>naissance</th>
+            <th>sexe</th>
+            <th>email</th>
         </tr>
         <tbody>
-        <?php
-            foreach($etudiants as $etudiant){
-                echo '<tr><td>'.$etudiant['nom'].'</td>';
-                echo '<td>'.$etudiant['prenom'].'</td>';
-                echo '<td>'.$etudiant['naissance'].'</td></tr>';
-            };
-        ?>
+        <?php foreach($student as $students): ?>
+            <tr>
+                <td></td>
+                <td><?=$students['prenom'];?></td>
+                <td><?=$students['nom'];?></td>
+                <td><?=$students['naissance'];?></td>
+                <td></td>
+                <td></td>
+            </tr>
+        <?php endforeach;?>
         </tbody>
     </thead>
 </table>
